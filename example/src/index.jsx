0785e5df6dc5f6495a4c35261f408987432b4d85
lindakai2016 <398017990@qq.com>
2018-06-21 11:32:07 +0800

    init

---
+++ example/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render} from 'react-dom';
+import Kline from '../../src';
+
+const App = () => (
+    <Kline />
+);
+
+render(<App />, document.getElementById("root"));
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './css/style.css';
+
+const Kline = () => (
+    <h1>Hello from My Component</h1>
+);
+export default Kline;
